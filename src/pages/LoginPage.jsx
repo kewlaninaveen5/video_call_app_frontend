039472eece17dc3d75f4622878d5c83d74d7cb59
Naveen Kewlani <kewlaninaveen5@gmail.com>
2025-10-28 21:22:05 +0530

    updated completion date

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div>[Project is still under production, will be ready by 5th Nov 2025] </div>
   )
 }
 
