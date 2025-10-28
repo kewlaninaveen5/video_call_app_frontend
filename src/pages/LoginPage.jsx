189ec45361dd06ae27bb5f3d4ba45fe3891befdf
Naveen Kewlani <kewlaninaveen5@gmail.com>
2025-10-28 21:58:22 +0530

    updated login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
+    <>
+    
     <div>[Project is still under production, will be ready by 5th Nov 2025] </div>
+    Until then, Please check my older projects here: 
+    <a href='https://sortingalgvisualizer.netlify.app/'> 
+      Sorting algorithm Visulaiser (DSA Focused Single Page Application)
+    </a>
+    <a href='https://py2jstranspiler.netlify.app/'>
+      Python to Javascript Transpiler (Core Software Development/Language Interpretation)
+    </a>
+    </>
   )
 }
 
