46be77669ca7417ae73ce854fdcbfe72ce74a33c
Naveen Kewlani <kewlaninaveen5@gmail.com>
2025-11-04 18:20:10 +0530

    final commit, leading to another page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const LoginPage = () => {
   return (
     <>
-    You are visiting this site from my old Resume. 
-    The project you are looking for is live at: <br/>
-    <a href='https://masterlingo.onrender.com/' className='font-mono to-blue-200' > 
-      Masterlingo : Learn language by Videocalling
-    </a>
+      You are visiting this site from my old Resume. The project you are looking
+      for is live at: <br />
+      <a
+        href="https://masterlingo.onrender.com/"
+        className="font-mono to-blue-200 underline"
+      >
+        Masterlingo : Learn languages by Videocalling
+      </a>
     </>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
